088a080ee8b7e49a9ca88aa9aa34509008aae59e
Jeanne Boyarsky <nyjeanne@gmail.com>
2019-06-17 19:27:28 -0400

    reproduce problem with *IT

---
+++ src/test/java/com/coderanch/JeanneIT.java
@@ -0,0 +1,18 @@
+package com.coderanch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class JeanneIT{
+
+	@Test
+	public void goodTest() {
+		
+	}
+
+	@Test
+	public void badTest() {
+		fail("bad");
+	}
+}
---
+++ src/test/java/com/coderanch/JeanneTest.java
@@ -0,0 +1,18 @@
+package com.coderanch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class JeanneTest {
+
+	@Test
+	public void goodTest() {
+		
+	}
+
+	@Test
+	public void badTest() {
+		fail("bad");
+	}
+}
